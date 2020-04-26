1b3f901d597f2cf36e3c8a7b6cbd5b382affe33e
lechinoix <nicolas.ngomai@gmail.com>
2020-04-26 17:33:50 +0200

    Updates

---
+++ precache-manifest.e7e0087f75e33f8d5adef2dae76eced9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5055b30297d64830696654ba741d50c1",
+    "url": "/fill-the-square/index.html"
+  },
+  {
+    "revision": "f1fce7e3f8325251aa0b",
+    "url": "/fill-the-square/static/css/main.2b20ecb1.chunk.css"
+  },
+  {
+    "revision": "94ac001b455c67d2554c",
+    "url": "/fill-the-square/static/js/2.daec04b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fill-the-square/static/js/2.daec04b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fce7e3f8325251aa0b",
+    "url": "/fill-the-square/static/js/main.65a1d03a.chunk.js"
+  },
+  {
+    "revision": "f70af3bcd2c1185b12a8",
+    "url": "/fill-the-square/static/js/runtime-main.59f64474.js"
+  }
+]);
